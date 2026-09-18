baf77c18e7c0af446a963b9092383ea0a03c0181
Aarin Nahar <aarinnahar@gmail.com>
2026-09-18 10:58:11 +0530

    Update api.py

--- api.py
+++ api.py
@@ -185,8 +185,7 @@ async def evaluate_document(
     # ---------------------------------------------------------
     # 7. Trigger LangGraph Orchestrator
     # ---------------------------------------------------------
-   try:
-        # AWAIT the async LangGraph execution
+    try:
         final_html_report = await run_evaluator(agent_state_input) 
         return final_html_report
     except Exception as e:
